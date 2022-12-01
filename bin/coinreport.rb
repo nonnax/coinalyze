0e39ff6e0c612954de5ae704d2cb4ab17f386dc7
nonnax <ironald@gmail.com>
2022-12-01 14:29:00 +0800

    refactor

--- bin/coinreport.rb
+++ bin/coinreport.rb
@@ -3,8 +3,18 @@
 # require 'daru'
 require 'rubytools/fzf'
 # IO.popen("./get.rb")
+center,=ARGV
+center||=14
+
+cmd=<<~___
+echo '1-day'
+&& harry_plotter.rb {}_1.csv | tail
+&& echo '#{center}-days'
+&& harry_plotter.rb {}_#{center}.csv | tail
+&& echo '90-days'
+&& harry_plotter.rb {}_90.csv | tail
+___
 f=Dir["*9*.csv"]
   .map{|f| File.basename(f, '.*')}
   .map{|f| f.gsub(/_.+/,'')}
-  .fzf_preview("echo '1-day' && bin/harry_plotter.rb {}_1.csv | tail && echo '7-days' && bin/harry_plotter.rb {}_7.csv | tail -n 22")
-  # .fzf_preview("echo '1-day' && ./harry_plotter.rb {}_1.csv | tail && echo '30-days' && ./harry_plotter.rb {}_30.csv | tail && echo '90-days' && ./harry_plotter.rb {}_90.csv | tail -n 20")
+  .fzf_preview(cmd.gsub(/\n+/, ' '))
