3c837338f9f4a416ae59c1a2aed93a22387b1348
nonnax <ironald@gmail.com>
2022-12-02 17:15:02 +0800

    cleanup

--- bin/coindesc.rb
+++ bin/coindesc.rb
@@ -3,5 +3,19 @@
 # require_relative 'coingecko_ohlc'
 require 'rubytools/fzf'
 
-%w[bitcoin-cash litecoin chainlink ripple uniswap]
-  .fzf_preview('harry_plotter.rb {}_1.csv | tail -n14 && echo 1 && daru_describe.rb {}_1.csv && echo 14 && daru_describe.rb {}_14.csv && echo 90 && daru_describe.rb {}_90.csv')
+def cmd
+  s=<<~___
+  harry_plotter.rb {}_1.csv | tail -n14
+  && echo 1
+  && daru_describe.rb {}_1.csv
+  && echo 14
+  && daru_describe.rb {}_14.csv
+  && echo 30
+  && daru_describe.rb {}_30.csv
+  && echo 90
+  && daru_describe.rb {}_90.csv
+  ___
+  s.gsub(/\n+/, ' ')
+end
+
+%w[bitcoin-cash litecoin chainlink ripple uniswap].fzf_preview(cmd)
--- bin/plotpv.rb
+++ bin/plotpv.rb
@@ -6,10 +6,10 @@ names=
 Dir["plots/h*#{coin}*"]
 .select{|f| f.match?(/\d/)}
 .reject{|f| f.match?(/14/)}
-.sort{|a, b| a.scan(/\d+/)[0].to_i<=>b.scan(/\d+/)[0].to_i}
+.sort{|a, b| a.scan(/\d+\./)[0].to_i<=>b.scan(/\d+\./)[0].to_i}
 
 names
 .map{|f| File.read(f).lines.last(n.to_i)}
 .map{|a| a.join }
 .then{|arr| names.map{|f| File.basename(f, '.*')}.zip(arr).to_h}
-.map{|k, v| puts [k.gsub(/hplot-/,''), v].join("\n")}
+.map{|k, v| puts [k.gsub(/hp-\d+/,''), v].join("\n")}
