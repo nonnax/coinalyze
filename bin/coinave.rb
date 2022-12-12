aa8c74e972800fdebe869f8219c02859e10edb03
nonnax <ironald@gmail.com>
2022-12-12 11:07:21 +0800

    add coinave.rg

---
+++ bin/coinave.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+# Id$ nonnax 2022-12-12 10:09:47
+require 'file/filer'
+require 'df/df_ext'
+require 'rubytools/numeric_ext'
+using DFExt
+
+def ave(f)
+ st=CSVFile.new(f)
+ c=Filer.read(st)
+
+ c.transpose.group_by(&:shift).transform_values(&:flatten).filter_map do |k, v|
+  [k, v.inject(:+)/v.size.to_f] unless k.match?(/date/)
+ end
+
+rescue
+  return
+end
+
+ARGV
+.sort{|a, b| a.scan(/\d+/).first.to_i<=>b.scan(/\d+/).first.to_i}.map do |f|
+  [[File.basename(f,'.*'), ave(f).to_h]].to_h
+end
+.inject({}){|acc, h| acc.merge(h.transform_values(&:values))}
+.map{|k, v| [k, v<<v.sum/v.size.to_f] }.to_h
+.to_flat_array
+.to_balanced_array
+.then{|arr| arr.prepend(%w[coin open high low close average])}
+.transpose
+.tap{|arr|
+  first=arr.shift
+  first<<'average'
+  arr.map{|r|
+    name=r.shift
+    v=r.sum/r.size.to_f
+    r.prepend(name)
+    r<<v
+    r
+  }
+  arr.prepend(first)
+}
+.to_table
+.then(&method(:puts))
--- bin/daru_summary.rb
+++ bin/daru_summary.rb
@@ -5,6 +5,8 @@ require 'daru'
 require 'rubytools/numeric_ext'
 require 'json'
 require 'df/df'
+require 'df/df_ext'
+using DFExt
 
 Filer.read(MarshalFile.new 'darudesc.mar').map.with_index{|(k, df), i|
  # pp df.methods.sort.each_slice(5).to_a if i.zero?
@@ -22,7 +24,17 @@ Filer.read(MarshalFile.new 'darudesc.mar').map.with_index{|(k, df), i|
 .then{|df| df[1,2]{|a, b| (b+a)/2 if [a, b].all?} }
 .then{|df| df[2,3]{|a, b| (b+a)/2 if [a, b].all?} }
 # .then{|df| puts df.to_s}
-.then{|df| puts df.to_s(index:true, columns: %w[id min mean max minmean meanmax])}
+.tap{|df| puts df.to_s(index:true, columns: %w[id min mean max minmean meanmax])}
+.then{|df|
+
+ puts df
+   .to_a
+   .group_by{|k| k.shift.scan(/[\D-]+/).first }
+   .transform_values(&:flatten)
+   .map{|k, v| [k, v.inject(:+)/v.size.to_f] rescue [0, 0] }
+   .to_balanced_array
+   .to_table
+}
 # .then(&JSON.method(:pretty_generate))
 # .then(&method(:puts))
 
